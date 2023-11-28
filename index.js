c888aa8d11b5c4b2d770d7a770c46251f1b01302
jagruti123 <jagrutinand19@gmail.com>
2023-11-28 15:36:29 +0530

    jaggu commint

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ function mul(){
     
 }
 console.log("amulya");
-<<<<<<< HEAD
-console.log("jagruti");
-
-function add(){
-    console.log("added new function")
-=======
 console.log("jagruti")
-if(age<20){
-    console.log("young")
-}else{
-    console.log("older")
->>>>>>> 8be97983e16d1841cd908e8652978cb4f7c18011
+function add(){
+    console.log("add function")
 }
